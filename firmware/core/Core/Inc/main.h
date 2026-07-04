09eab94a0a6d9675a224c81644b6c021df8ad334
AssoDePicche <samuelprado730@gmail.com>
2026-07-04 14:57:52 -0300

    feat: OLED integration

--- firmware/core/Core/Inc/main.h
+++ firmware/core/Core/Inc/main.h
@@ -31,7 +31,10 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include  <stdio.h>
 
+#include "ssd1306.h"
+#include "ssd1306_fonts.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
